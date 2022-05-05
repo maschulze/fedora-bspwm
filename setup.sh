b55cbb5d24a8b37a874648c692000d0be5c977c0
maschulze <maschulze@users.noreply.github.com>
2022-05-05 19:12:39 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply https://github.com/maschulze/fedora-bspwm-dotfiles
+
+sudo dnf update
+
+sudo dnf install git vim bspwm sddm picom thunar kitty nitrogen polybar sxhkd rofi fontawesome-fonts fontawesome-fonts-web
+firefox arandr alss-utils lxpolkit pavucontrol light pipewire
+
+sudo systemctl enable sddm
+sudo systemctl set-default graphical-target
