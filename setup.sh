81129cdecfe0281323cfc9070a1768719db5b7d4
maschulze <maschulze@users.noreply.github.com>
2022-05-05 19:20:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@ sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply https://github.com/maschulz
 
 sudo dnf update
 
-sudo dnf install git vim bspwm sddm picom thunar kitty nitrogen polybar sxhkd rofi fontawesome-fonts fontawesome-fonts-web
-firefox arandr alss-utils lxpolkit pavucontrol light pipewire
+sudo dnf install git vim bspwm sddm picom thunar kitty nitrogen polybar sxhkd rofi fontawesome-fonts fontawesome-fonts-web firefox arandr alss-utils lxpolkit pavucontrol light pipewire
 
 sudo systemctl enable sddm
-sudo systemctl set-default graphical-target
+sudo systemctl set-default graphical.target
